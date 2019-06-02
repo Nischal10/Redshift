886238de84416f0448206f115c4675e51615bce3
Nischal10 <nis.kharel@gmail.com>
2019-06-02 12:24:19 +0545

    Small code fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:redshift/uiElemenets/bottombar.dart';
 import 'package:redshift/uiElemenets/nearbycard.dart';
 
 void main() => runApp(MyApp());
@@ -28,28 +27,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _selectedIndex = 0;
-static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-static const List<Widget> _widgetOptions = <Widget>[
-  Text(
-    'Index 0: Home',
-    style: optionStyle,
-  ),
-  Text(
-     'Index 1: Business',
-     style: optionStyle,
-  ),
-  Text(
-     'Index 2: School',
-     style: optionStyle,
-  ),
-];
-
-void _onItemTapped(int index) {
-  setState(() {
-    _selectedIndex = index;
-  });
-}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,24 +56,6 @@ void _onItemTapped(int index) {
           ],
         ),
       ),
-    bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          title: Text('Business'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          title: Text('School'),
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.amber[800],
-      onTap: _onItemTapped,
-    ),);
+    );
   }
 }
