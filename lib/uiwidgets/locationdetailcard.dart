deb453c06928983c0be5e06ddc3cd2433237ad90
Nischal10 <nis.kharel@gmail.com>
2019-06-07 22:07:17 +0545

    Working on locataioncard

--- lib/uiwidgets/locationdetailcard.dart
+++ lib/uiwidgets/locationdetailcard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LocationDetail extends StatefulWidget {
   @override
@@ -9,11 +10,109 @@ class _LocationDetailState extends State<LocationDetail> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.80,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.80,
+        height: 250,
+        decoration: BoxDecoration(
           color: Colors.blue,
-          height: 250,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.blue[100],
+              offset: Offset(0.0, 4.0),
+              blurRadius: 10.0,
+              spreadRadius: 0.0,
+            ),
+          ],
         ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Stats',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    'Review',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    'About',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.pray,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Temples',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Text('Patan Durbar Square',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            )),
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
