42de500ef80c016ec00e2e53c6eea4d58484ce7c
Nischal10 <nis.kharel@gmail.com>
2019-06-20 14:10:46 +0545

    Splash Screen Changes

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:redshift/assets/design_screenSize.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:redshift/assets/assets.dart';
+
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -19,27 +21,94 @@ class _SplashScreenState extends State<SplashScreen> {
         allowFontScaling: true)
       ..init(context);
     return Material(
-      child: Container(
-        width: ScreenUtil.getInstance().setWidth(ScreenSize.screenWidth),
-        height: ScreenUtil.getInstance().setHeight(ScreenSize.screenHeight),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0, 1],
-            colors: [
-              Color(0xffff5e62),
-              Color(0xffff9966),
-            ],
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil.getInstance().setWidth(ScreenSize.screenWidth),
+            height: ScreenUtil.getInstance().setHeight(ScreenSize.screenHeight),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0, 1],
+                colors: [
+                  Color(0xffff5e62),
+                  Color(0xffff9966),
+                ],
+              ),
+            ),
+            child: FlareActor(
+              'assets/animations/redshift.flr',
+              alignment: Alignment.bottomCenter,
+              fit: BoxFit.cover,
+              animation: _animationName,
+              isPaused: false,
+            ),
           ),
-        ),
-        child: FlareActor(
-          'assets/animations/redshift.flr',
-          alignment: Alignment.bottomCenter,
-          fit: BoxFit.cover,
-          animation: _animationName,
-          isPaused: false,
-        ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Container(
+              width: ScreenUtil.getInstance().setWidth(ScreenSize.screenWidth),
+              height: ScreenUtil.getInstance().setHeight(400),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Redshift',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Material(
+                    type: MaterialType.transparency,
+                    child: InkWell(
+                      splashColor: Colors.red,
+                      onTap: () {
+                      },
+                      child: Container(
+                        height: 48,
+                        width: ScreenUtil.getInstance().setHeight(812),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(0, 0, 0, 0.2),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(12),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                FontAwesomeIcons.google,
+                                color: Colors.white,
+                                size: 24,
+                              ),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text(
+                                'Sign in with Google',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
