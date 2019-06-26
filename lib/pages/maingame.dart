bd29cb1a8dd6dd522e9a7884090add8a08f959d9
bprayush <bprayush@yahoo.com>
2019-06-26 17:19:35 +0545

    .

--- lib/pages/maingame.dart
+++ lib/pages/maingame.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import 'package:redshift/models/user.dart';
+import 'package:redshift/uiwidgets/game_app_bar/game_app_bar.dart';
 
 // https://medium.com/flutter-community/build-a-location-service-in-flutter-367a1b212f7a
 // Current Implementation(uses geolocater plugin): https://github.com/rajayogan/flutter-googlemaps-geolocation/blob/master/lib/main.dart
@@ -63,6 +67,35 @@ class _MapPageState extends State<MapPage> {
                       'loading please wait',
                     ),
                   );
+              },
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance
+                  .collection('user-data')
+                  .where('uid', isEqualTo: Provider.of<User>(context).uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  Map<String, dynamic> userData =
+                      snapshot.data.documents.first.data;
+                  return GameAppBar(
+                    badge: userData['badges'],
+                    coin: userData['coins'],
+                    level: userData['level'],
+                    levelProgress: userData['points'].toDouble(),
+                  );
+                }
+                return GameAppBar(
+                  badge: 0,
+                  coin: 0,
+                  level: 1,
+                  levelProgress: 0,
+                );
               },
             ),
           ),
